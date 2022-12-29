4e417f38f6465ab4fae9631b92a749f99069447e
YumaHozumi <cpucmptrpython72@gmail.com>
2022-12-29 12:17:28 +0900

    APIから情報を取得できなかったときの処理追加

--- Prototype/book-sns/src/main/resources/static/js/script.js
+++ Prototype/book-sns/src/main/resources/static/js/script.js
@@ -9,9 +9,14 @@ function show(){
 		//let contents = json[0].onix.CollateralDetail.TextContent;
 		content = json[0].onix.CollateralDetail.TextContent[0];
 		let pElement = document.getElementById(book.bookId);
-		console.log(pElement);
 		pElement.innerText = content.Text;
-	});
+	})
+	.catch(e => {
+		console.log(e);
+		let errorElement = document.getElementById(book.bookId);
+		errorElement.innerText = 'APIから情報を取得できませんでした';
+	})
+	
 		
 	});
 }
